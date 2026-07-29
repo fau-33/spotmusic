a38e9e4044aa2d06641ff83d6bc3e101cde354b1
Flávio Félix <fau_felix30@hotmail.com>
2026-07-28 21:26:11 -0300

    chore: initial project setup with Spring Boot and dependencies

---
+++ spotmusic/src/test/java/br/com/flaviofelix/spotmusic/SpotmusicApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.flaviofelix.spotmusic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpotmusicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
