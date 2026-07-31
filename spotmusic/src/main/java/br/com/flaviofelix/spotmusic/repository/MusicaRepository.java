4bbc17c9feb9237b3352fc8bdd99e4cd5dc2095d
Flávio Félix <fau_felix30@hotmail.com>
2026-07-31 13:36:12 -0300

    feat: add Playlist and Musica repositories using Spring Data JPA

---
+++ spotmusic/src/main/java/br/com/flaviofelix/spotmusic/repository/MusicaRepository.java
@@ -0,0 +1,7 @@
+package br.com.flaviofelix.spotmusic.repository;
+
+import br.com.flaviofelix.spotmusic.domain.Musica;
+import org.springframework.data.repository.Repository;
+
+public interface MusicaRepository extends Repository<Musica, Long> {
+}
---
+++ spotmusic/src/main/java/br/com/flaviofelix/spotmusic/repository/PlaylistRepository.java
@@ -0,0 +1,8 @@
+package br.com.flaviofelix.spotmusic.repository;
+
+import br.com.flaviofelix.spotmusic.domain.Playlist;
+import org.springframework.data.repository.Repository;
+
+public interface PlaylistRepository extends Repository<Playlist, Long> {
+
+}
