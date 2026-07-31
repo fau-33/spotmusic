a44524795407122f07ab207f9250b238a6d3aa84
Flávio Félix <fau_felix30@hotmail.com>
2026-07-31 14:27:03 -0300

    feat: add /home route to HomeController

--- spotmusic/src/main/java/br/com/flaviofelix/spotmusic/controller/HomeController.java
+++ spotmusic/src/main/java/br/com/flaviofelix/spotmusic/controller/HomeController.java
@@ -6,11 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-  // Quando alguém acessar o site principal (a barra "/"), faça isso:
-  @GetMapping("/")
+  @GetMapping({"/", "/home"})
   public String exibirHomePage() {
-
-    // Retorna exatamente o nome do arquivo HTML (sem o ".html")
     return "home";
   }
 }
