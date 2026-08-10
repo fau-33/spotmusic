ed4ad1128b7b8eac4a7a24aaf07393260587365a
Flávio Félix <fau_felix30@hotmail.com>
2026-08-10 12:42:51 -0300

    feat: complete Musica CRUD with repository, service, controller and views

--- spotmusic/src/main/java/br/com/flaviofelix/spotmusic/repository/MusicaRepository.java
+++ spotmusic/src/main/java/br/com/flaviofelix/spotmusic/repository/MusicaRepository.java
@@ -2,6 +2,13 @@ package br.com.flaviofelix.spotmusic.repository;
 
 import br.com.flaviofelix.spotmusic.domain.Musica;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.List;
 
 public interface MusicaRepository extends JpaRepository<Musica, Long> {
+
+  // Busca todas as músicas que pertencem a uma playlist específica
+  List<Musica> findByPlaylistId(long playlistId);
+
+  // Busca uma música específica dentro de uma playlist específica
+  Musica findByIdAndPlaylistId(long id, long playlistId);
 }
--- spotmusic/src/main/java/br/com/flaviofelix/spotmusic/service/MusicaService.java
+++ spotmusic/src/main/java/br/com/flaviofelix/spotmusic/service/MusicaService.java
@@ -1,39 +1,49 @@
 package br.com.flaviofelix.spotmusic.service;
 
 import br.com.flaviofelix.spotmusic.domain.Musica;
+import br.com.flaviofelix.spotmusic.domain.Playlist;
 import br.com.flaviofelix.spotmusic.repository.MusicaRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Service
-@Transactional
 public class MusicaService {
 
   @Autowired
   private MusicaRepository musicaRepository;
 
-  public void salvar(Musica musica) {
+  @Autowired
+  private PlaylistService playlistService;
+
+  public List<Musica> recuperarPorPlaylist(long playlistId) {
+    return musicaRepository.findByPlaylistId(playlistId);
+  }
+
+  public Musica recuperarPorPlaylistIdEMusicaId(long playlistId, long musicaId) {
+    return musicaRepository.findByIdAndPlaylistId(musicaId, playlistId);
+  }
+
+  public void salvar(Musica musica, long playlistId) {
+    // 1. Busca a playlist no banco de dados
+    Playlist playlist = playlistService.recuperarPorId(playlistId);
+    // 2. Avisa que essa música pertence a essa playlist
+    musica.setPlaylist(playlist);
+    // 3. Salva a música
     musicaRepository.save(musica);
   }
 
-  @Transactional(readOnly = true)
-  public List<Musica> recuperar() {
-    return musicaRepository.findAll();
-  }
-
-  @Transactional(readOnly = true)
-  public Musica recuperarPorId(long id) {
-    return musicaRepository.findById(id).orElse(null);
-  }
-
-  public void atualizar(Musica musica) {
+  public void atualizar(Musica musica, long playlistId) {
+    Playlist playlist = playlistService.recuperarPorId(playlistId);
+    musica.setPlaylist(playlist);
     musicaRepository.save(musica);
   }
 
-  public void excluir(long id) {
-    musicaRepository.deleteById(id);
+  public void excluir(long playlistId, long musicaId) {
+    Musica musica = recuperarPorPlaylistIdEMusicaId(playlistId, musicaId);
+    if (musica != null) {
+      musicaRepository.delete(musica);
+    }
   }
 }
